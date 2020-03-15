15626f8b2ed3a6b6cf5cb16a2b258ea6d64d8d05
bryan-elliott.tam.1@ulaval.ca <bryan-elliott.tam.1@ulaval.ca>
2020-03-14 22:10:08 -0400

    testing framework work

--- src/test/util.test.js
+++ src/test/util.test.js
@@ -1,20 +1,51 @@
 import "./../util/util.js";
+import { save } from "./../util/util.js";
 const sinon = require("sinon");
+const expect = require("chai").expect;
 
-const test = () => {
-  return document.createElement("a");
-};
-const mock = sinon.mock(document);
-mock
-  .expects("createElement")
-  .withExactArgs("a")
-  .returns(3);
-const assert = require("assert");
+describe("test the function of util", function() {
+  describe("test the save(cards, name3x3, username, storage) function", function() {
+    it("given a card, a name3x3, and username and a storage when saving then the storage should return the card data, the username and the 3x3 name at the data item", function() {
+      const id = "3";
+      const attribute = "boo";
 
-describe("Array", function() {
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal(test(), 3);
+      const card = makeCard(id, attribute);
+      const cards = [card];
+
+      const username = "foo";
+      const name3x3 = "bar";
+      const storage = makeStorage();
+
+      const expectedValue = {
+        name3x3: name3x3,
+        username: username,
+        card: { [id]: attribute }
+      };
+
+      save(cards, name3x3, username, storage);
+      expect(JSON.parse(storage.getItem("data"))).to.deep.eql(expectedValue);
     });
   });
 });
+
+const makeCard = (id, attribute) => {
+  const card = { id: "" };
+  card.getAttribute = () => {};
+  sinon
+    .stub(card, "getAttribute")
+    .callThrough()
+    .withArgs("data")
+    .returns(attribute);
+  sinon.stub(card, "id").value(id);
+  return card;
+};
+const makeStorage = () => {
+  const storage = { data: "" };
+  storage.setItem = (property, data) => {
+    this[property] = data;
+  };
+  storage.getItem = data => {
+    return this[data];
+  };
+  return storage;
+};
