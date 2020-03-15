c66a5d491ad5bdad7c2b02679e8fb2f3f5785a1c
bryan-elliott.tam.1@ulaval.ca <bryan-elliott.tam.1@ulaval.ca>
2020-03-15 19:04:32 -0400

    karma added start to test webcomponent

---
+++ src/test/component/AnimeCard.test.js
@@ -0,0 +1,10 @@
+const sinon = require("sinon");
+const expect = require("chai").expect;
+import "./jsdomConfig.js";
+import "./../../component/AnimeCard.js";
+
+describe("test of the AnimeCard html custom component", () => {
+  it("aaa", () => {
+    console.log(document);
+  });
+});
--- src/test/util.test.js
+++ src/test/util.test.js
@@ -4,7 +4,7 @@ const sinon = require("sinon");
 const expect = require("chai").expect;
 
 describe("test the function of util", function() {
-  describe("test the save(cards, name3x3, username, storage) function", function() {
+  describe("test the save(cards, name3x3, username, storage)  and load(cards, name3x3, username, storage) function", function() {
     it("given a card, a name3x3, and username and a storage when saving then the storage should return the card data, the username and the 3x3 name at the data item", function() {
       const id = "3";
       const attribute = "boo";
@@ -22,6 +22,44 @@ describe("test the function of util", function() {
         card: { [id]: attribute }
       };
 
+      save(cards, name3x3, username, storage);
+      expect(JSON.parse(storage.getItem("data"))).to.deep.eql(expectedValue);
+    });
+    it("given no card when saving the storage should contain only the 3x3name, the username and an empty card attribue", function() {
+      const cards = [];
+
+      const username = "foo";
+      const name3x3 = "bar";
+      const storage = makeStorage();
+
+      const expectedValue = {
+        name3x3: name3x3,
+        username: username,
+        card: {}
+      };
+      save(cards, name3x3, username, storage);
+      expect(JSON.parse(storage.getItem("data"))).to.deep.eql(expectedValue);
+    });
+    it("given multiple cards, a name3x3, and username and a storage when saving then the storage should return the card data, the username and the 3x3 name at the data item", function() {
+      const id = ["3", "2", "1"];
+      const attribute = ["boo", "coo", "moo"];
+
+      const cards = id.map((el, i) => {
+        return makeCard(el, attribute[i]);
+      });
+
+      const username = "foo";
+      const name3x3 = "bar";
+      const storage = makeStorage();
+
+      const expectedValue = {
+        name3x3: name3x3,
+        username: username,
+        card: {}
+      };
+      id.forEach((el, i) => {
+        expectedValue.card[el] = attribute[i];
+      });
       save(cards, name3x3, username, storage);
       expect(JSON.parse(storage.getItem("data"))).to.deep.eql(expectedValue);
     });
